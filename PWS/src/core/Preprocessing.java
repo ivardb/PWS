4964f924c32439b69f336724cee1ee169c088385
jaapdejong15 <jaapdejong15@gmail.com>
2017-09-19 14:01:59 +0200

    created preprocessing

--- PWS/src/core/Preprocessing.java
+++ PWS/src/core/Preprocessing.java
@@ -1,9 +1,20 @@
 package core;
 
-public class Preprocessing {
-	public static Tensor preproces() {
+import java.awt.image.BufferedImage;
+
+public class Preprocessing 
+{	
+	private static final int IMAGE_WIDTH = 256;
+	private static final int IMAGE_HEIGHT = 256;
 	
+	public static Tensor preproces() 
+	{
+		return null;
 	}
 	
-	private static BufferedImage 
+	private static BufferedImage scale(BufferedImage input_image)
+	{
+		
+		return null;
+	}
 }
