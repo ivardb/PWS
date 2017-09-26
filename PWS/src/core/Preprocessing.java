de636afd5d436e446555a431178d918106f8c06a
ivardb <ivardb@gmail.com>
2017-09-26 13:33:46 +0200

    preprocess update

--- PWS/src/core/Preprocessing.java
+++ PWS/src/core/Preprocessing.java
@@ -22,7 +22,7 @@ public class Preprocessing
 			int x_begin = (width - IMAGE_WIDTH)/2;
 			input_image = crop(input_image, x_begin, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
 		}
-		else
+		else if(width/height < IMAGE_WIDTH / IMAGE_HEIGHT)
 		{
 			//too small
 			double scale = IMAGE_WIDTH/width;
@@ -31,6 +31,12 @@ public class Preprocessing
 			int y_begin = (height - IMAGE_WIDTH)/2;
 			input_image = crop(input_image, 0, y_begin, IMAGE_WIDTH, IMAGE_HEIGHT);
 		}
+		/*int[][] pixels = new int[IMAGE_WIDTH][IMAGE_HEIGHT];
+
+		for( int i = 0; i < IMAGE_WIDTH; i++ )
+		    for( int j = 0; j < IMAGE_HEIGHT; j++ )
+		        pixels[i][j] = input_image.getRGB( i, j );
+		        Maarten is dit de goed pixel array snap geen zak van je code*/
 		return null;
 	}
 	
