aaaefa688d607ffafba92e12c83631bff692bcee
ivardb <ivardb@gmail.com>
2017-12-18 09:44:25 +0100

    Training class fixes

--- PWS/src/core/Training.java
+++ PWS/src/core/Training.java
@@ -10,17 +10,33 @@ public class Training {
 	private String image_path = "F:\\Fotos_AI\\gt_db";
 	Random random = new Random();
 	
-	public void Train(String path_map, int amount_of_runs) {
+	public void Train(int amount_of_runs) {
 		int subject = random.nextInt(number_of_testsubjects) + 1;
 		int train_subject;
+		String strSubject;
+		String strTrain_subject;
 		for(int i = 0; i<amount_of_runs; i++) {
 			if(random.nextInt(2) == 1) {
 				train_subject = subject;
 			} else {
 				train_subject = -1;
 			}
-			String path = getNextPath(Integer.toString(train_subject));
-			Tensor loaded_tensor = loadTrainingset(path);
+			if(subject < 10 && subject!=-1) {
+				strSubject = "0" + subject;
+			} else {
+				strSubject = Integer.toString(subject);
+			}
+			
+			if(train_subject < 10 && train_subject != -1) {
+				strTrain_subject = "0" + train_subject;
+			} else {
+				strTrain_subject = Integer.toString(train_subject);
+			}
+			
+			String first_path = getNextPath(strSubject);
+			String second_path = getNextPath(strTrain_subject);
+			Tensor first_tensor = loadTrainingset(first_path);
+			Tensor second_tensor = loadTrainingset(second_path);
 			
 			//insert code for running a tensor through ai and backpropagation
 			
@@ -41,13 +57,13 @@ public class Training {
 	private String getNextPath(String subject) {
 		String picture_string;
 		if(subject == "" || subject == "-1") {
-			subject = "s" + random.nextInt(50) + 1;					
+			subject = "s" + random.nextInt(number_of_testsubjects) + 1;					
 		} else if(subject.substring(0,1) != "s") {
 			subject = "s" + subject;
 		}
 		int picture = random.nextInt(number_of_pictures) + 1;
 		if(picture < 10) {
-			picture_string = "0" + picture;
+			picture_string = "0" + picture+".jpeg";
 		} else {
 			picture_string = Integer.toString(picture);
 		}
@@ -67,4 +83,7 @@ public class Training {
 		number_of_pictures = number;
 	}
 		
+	public void setDatabasePath(String path) {
+		image_path = path;
+	}
 }
